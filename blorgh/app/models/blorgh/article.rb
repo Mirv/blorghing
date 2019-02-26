65b0633b03b525d623bbc0b8c0639d008551d285
Mirv <reski.raptor@gmail.com>
2019-02-26 15:12:29 +0000

    4_4 working long form, next we shortcut it

--- blorgh/app/models/blorgh/article.rb
+++ blorgh/app/models/blorgh/article.rb
@@ -3,13 +3,15 @@ module Blorgh
     has_many :comments
     
     attr_accessor :author_name
-    belongs_to :author, class_name: "User"
+    # belongs_to :author, class_name: "User"
+    belongs_to :author, class_name: Blorgh.author_class
      
     before_validation :set_author
      
     private
       def set_author
-        self.author = User.find_or_create_by(name: author_name)
+        # self.author = User.find_or_create_by(name: author_name)
+        self.author = Blorgh.author_class.constantize.find_or_create_by(name: author_name)
       end
   end
 end
--- blorgh/lib/blorgh.rb
+++ blorgh/lib/blorgh.rb
@@ -1,4 +1,5 @@
 require "blorgh/engine"
 
 module Blorgh
+  mattr_accessor :author_class
 end
