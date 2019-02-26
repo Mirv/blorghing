faaedf49dbf00065f75e8513e1c99c97590f2d56
Mirv <reski.raptor@gmail.com>
2019-02-26 15:20:16 +0000

    4_4 working - unrelated noticed authorname is set, but doesn't show in article#edit

--- blorgh/app/models/blorgh/article.rb
+++ blorgh/app/models/blorgh/article.rb
@@ -4,14 +4,16 @@ module Blorgh
     
     attr_accessor :author_name
     # belongs_to :author, class_name: "User"
-    belongs_to :author, class_name: Blorgh.author_class
+    # belongs_to :author, class_name: Blorgh.author_class
+    belongs_to :author, class_name: Blorgh.author_class.to_s
      
     before_validation :set_author
      
     private
       def set_author
         # self.author = User.find_or_create_by(name: author_name)
-        self.author = Blorgh.author_class.constantize.find_or_create_by(name: author_name)
+        # self.author = Blorgh.author_class.constantize.find_or_create_by(name: author_name)
+        self.author = Blorgh.author_class.find_or_create_by(name: author_name)
       end
   end
 end
--- blorgh/lib/blorgh.rb
+++ blorgh/lib/blorgh.rb
@@ -2,4 +2,8 @@ require "blorgh/engine"
 
 module Blorgh
   mattr_accessor :author_class
+  
+  def self.author_class
+    @@author_class.constantize
+  end
 end
